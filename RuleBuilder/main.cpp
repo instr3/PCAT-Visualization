796a2546a6009d4a392abf4486ff6fbbf624372f
instr3 <instr3@163.com>
2017-10-16 14:28:44 +0800

    Environment setup

---
+++ RuleBuilder/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int yyparse();
+extern FILE* yyin;
+
+int main(int argc, char* args[]) {
+	if (argc > 1) {
+		FILE *file = fopen("test.txt", "r");
+		if (!file) {
+			cerr << "Can't open file" << endl;
+			return 1;
+		}
+		else {
+			yyin = file;
+		}
+	}
+
+	yyparse();
+	return 0;
+}
