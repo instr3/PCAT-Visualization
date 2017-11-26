8d1145eaaf5eaccbb236fb8332879923994f4ad4
instr3 <instr3@163.com>
2017-11-26 16:33:57 +0800

    Add missing file

---
+++ CodeBlock/CompilerInvoker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CodeBlock
+{
+    class CompilerInvoker
+    {
+        public static string Compile(string src)
+        {
+            string fileName = "__temp.pcat";
+            string exeName = "RuleBuilder.exe";
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                sw.Write(src);
+            }
+            using (Process process = new Process())
+            {
+                ProcessStartInfo p = new ProcessStartInfo();
+                p.FileName = exeName;
+                p.Arguments = fileName + " -i";
+                p.RedirectStandardOutput = true;
+                p.UseShellExecute = false;
+                process.StartInfo = p;
+                StringBuilder output = new StringBuilder();
+                using (AutoResetEvent outputWaitHandle = new AutoResetEvent(false))
+                {
+                    process.OutputDataReceived += (sender, e) =>
+                    {
+                        if (e.Data == null)
+                        {
+                            outputWaitHandle.Set();
+                        }
+                        else
+                        {
+                            output.AppendLine(e.Data);
+                        }
+                    };
+                    process.Start();
+                    process.BeginOutputReadLine();
+                    process.WaitForExit();
+                    outputWaitHandle.WaitOne();
+                    if (process.ExitCode != 0)
+                    {
+                        MessageBox.Show(output.ToString());
+                        throw new ArgumentException("Compile Error");
+                    }
+                    return output.ToString();
+                }
+            }
+        }
+    }
+}
